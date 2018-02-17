51fb2cf34bd3cb475b786cd459ecf5987035678c
Kalpa Gunarathna <chathuranga456@gmail.com>
2018-02-17 16:09:02 +0530

    functions to determine ip address, ip version, MTU of a given interface

--- connection.go
+++ connection.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func StartConnection(network, address string) *net.UDPConn {
@@ -32,4 +33,58 @@ func ReadData(udpConn *net.UDPConn, buffer []byte) {
 	}
 
 	fmt.Println(string(buffer[0:recvBytes]))
+}
+
+// Returns MTU of the given network interface.
+func MTU(ifAddr string) int {
+	ifAddrs, err := net.Interfaces()
+
+	if err != nil {
+		log.Fatalf("Error in figuring out interfaces for system", err)
+	}
+
+	for _, i := range ifAddrs {
+		if i.Name == ifAddr {
+			return i.MTU
+		}
+	}
+	return 1024
+}
+
+// Returns IP address assigned to a given network interface
+func AssignedIP(ifAddr string) net.IP {
+	ifAddrs, err := net.Interfaces()
+
+	if err != nil {
+		log.Fatalf("Error in figuring out interfaces for system", err)
+	}
+
+	for _, i := range ifAddrs {
+		if i.Name == ifAddr {
+			// Get all unicast addresses for i
+			addrs, err := i.Addrs()
+
+			if err != nil {
+				log.Fatalf("Error in figuring out address for interface", err)
+			}
+
+			for _, addr := range addrs {
+				ip, _, err := net.ParseCIDR(addr.String())
+
+				if err != nil {
+					log.Fatalf("Error in figuring out address for interface", err)
+				}
+
+				if IsIPv4(&ip) {
+					return ip
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// If IP address is ipv4 type, returns true. Otherwise false.
+func IsIPv4(ip *net.IP) bool {
+	return strings.Contains(ip.String(), ".")
 }
